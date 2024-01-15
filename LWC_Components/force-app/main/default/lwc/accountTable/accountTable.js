2864778bd9cdf91948d3cfe116c5519eac3a2350
Recep EREN <qreceperen@gmail.com>
2024-01-15 11:08:58 -0500

    account table is prepared

--- LWC_Components/force-app/main/default/lwc/accountTable/accountTable.js
+++ LWC_Components/force-app/main/default/lwc/accountTable/accountTable.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from "lwc";
+import getAccounts from "@salesforce/apex/accountController.getAccounts";
 
-export default class AccountTable extends LightningElement {}
+export default class AccountTable extends LightningElement {
+  accounts;
+
+  columns = [
+    { label: "Account Name", fieldName: "Name" },
+    { label: "Account Type", fieldName: "Type" }
+  ];
+
+  @wire(getAccounts)
+  wiredAccounts({ error, data }) {
+    if (data) {
+      this.accounts = data;
+    } else if (error) {
+      console.log(error);
+    }
+  }
+}
