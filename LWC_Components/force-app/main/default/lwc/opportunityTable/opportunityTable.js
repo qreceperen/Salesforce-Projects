e18e615014a4f2044ea1c0251de2bd6dc856f78f
Recep Eren <qreceperen@gmail.com>
2024-01-15 10:07:46 -0500

    .

--- LWC_Components/force-app/main/default/lwc/opportunityTable/opportunityTable.js
+++ LWC_Components/force-app/main/default/lwc/opportunityTable/opportunityTable.js
@@ -1,24 +1,57 @@
-import { LightningElement, wire, track } from "lwc";
-import getOpportunities from "@salesforce/apex/OpportunityController.getOpportunities";
-
-const columns = [
-    { label: 'Opportunity Name', fieldName: 'Name' },
-    { label: 'Stage', fieldName: 'StageName' },
-    { label: 'Close Date', fieldName: 'CloseDate', type:'date'},
-    { label: 'Expected Amount', fieldName: 'ExpectedRevenue', type: 'currency' },
-];
+import { LightningElement, wire, track } from 'lwc';
+import getOpportunities from '@salesforce/apex/OpportunityController.getOpportunities';
+import getTotalOpportunitiesCount from '@salesforce/apex/OpportunityController.getTotalOpportunitiesCount';
 
 export default class OpportunityTable extends LightningElement {
-  @track opportunities;
-  columns = columns;
+    @track currentPage = 1;
+    @track pageSize = 5;
+    @track totalRecords;
+    @track totalPages;
+    opportunities;
 
-  @wire(getOpportunities)
-  wiredOpportunities({ error, data }) {
-    if (data) {
-      this.opportunities = data;
-      console.log("Received: ", data);
-    } else if (error) {
-      console.log(error);
+    columns = [
+        { label: 'Opportunity Name', fieldName: 'Name' },
+        { label: 'Stage', fieldName: 'StageName' },
+        { label: 'Close Date', fieldName: 'CloseDate', type: 'date' },
+        { label: 'Expected Amount', fieldName: 'ExpectedRevenue', type: 'currency' }
+    ];
+
+    @wire(getOpportunities, { pageNumber: '$currentPage', pageSize: '$pageSize' })
+    wiredOpportunities({ error, data }) {
+        if (data) {
+            this.opportunities = data;
+        } else if (error) {
+            console.error(error);
+        }
     }
-  }
+
+    @wire(getTotalOpportunitiesCount)
+    wiredTotalCount({ error, data }) {
+        if (data) {
+            this.totalRecords = data;
+            this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    get isFirstPage() {
+        return this.currentPage <= 1;
+    }
+
+    get isLastPage() {
+        return this.currentPage >= this.totalPages;
+    }
+
+    nextPage() {
+        if (this.currentPage < this.totalPages) {
+            this.currentPage++;
+        }
+    }
+
+    previousPage() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+        }
+    }
 }
