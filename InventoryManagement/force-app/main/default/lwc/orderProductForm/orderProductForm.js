90efb6ad3c66b36865fbf34be67d75d0338f339f
Recep EREN <qreceperen@gmail.com>
2024-11-07 20:28:35 -0500

    .

--- InventoryManagement/force-app/main/default/lwc/orderProductForm/orderProductForm.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api, track } from 'lwc';
-
-export default class OrderProductForm extends LightningElement {
-    @api productOptions = [];
-    @track selectedProductId = '';
-    @track orderQuantity;
-
-    
-}
